11c01c97de65c63cf2f46a4cba1b37c1a644066d
judah059 <ppddee777@gmail.com>
2022-04-05 17:07:57 +0300

    client and valet models

--- spray/users/admin.py
+++ spray/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from spray.users.models import User
+from spray.users.models import User, Client, Valet
 
 admin.site.register(User)
+admin.site.register(Client)
+admin.site.register(Valet)
 # Register your models here.
