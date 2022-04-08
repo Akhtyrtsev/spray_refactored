9bd083fa989e6d2618870f0c552590cf8dafa2ab
Serhii.Klimushyn <serhii.klimushyn@smissltd.com>
2022-04-08 22:59:23 +0300

    fix auth urls

--- spray/api/urls.py
+++ spray/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from spray.api.v1.authentication import urls as authentication_urls
 from spray.api.v1.users import urls as users_urls
+from drf_social_oauth2 import urls
 
 app_name = "api"
 
@@ -11,7 +12,7 @@ urlpatterns = [
     path('registration/', include('rest_auth.registration.urls')),
     # ----------------------- authentication ----------------------- #
     # ------------------------------------------------------------- #
-    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),  # OAuth2 social authentication
+    path("", include(authentication_urls)),
     # -------------------------- users -------------------------- #
     # ------------------------------------------------------------- #
     path("", include(users_urls)),
