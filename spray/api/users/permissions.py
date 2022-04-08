a25d8667f25f22bc566a542b451219f4b0310f3a
judah059 <ppddee777@gmail.com>
2022-04-08 15:42:32 +0300

    fixed str

--- spray/api/users/permissions.py
+++ spray/api/users/permissions.py
@@ -7,7 +7,6 @@ class IsValet(permissions.BasePermission):
     message = "Permission denied"
 
     def has_permission(self, request, view):
-        print(Client.objects.get(first_name='Test').pk, '!!!!!')
         if Valet.objects.filter(pk=request.user.pk, email=request.user.email).exists():
             return True
         else:
--- spray/api/users/serializers.py
+++ spray/api/users/serializers.py
@@ -7,18 +7,50 @@ from spray.users.models import Client, User, Valet
 class ClientGetSerializer(ModelSerializer):
     class Meta:
         model = Client
-        fields = ('first_name', 'last_name', 'email', 'phone', 'avatar_url', 'notification_email',
-                  'notification_sms', 'notification_push', 'stripe_id', 'apple_id', 'docusign_envelope',
-                  'customer_status', 'referal_code', 'notification_text_magic', 'is_phone_verified', 'is_new',
-                  'is_blocked')
+        fields = ('first_name',
+                  'last_name',
+                  'email',
+                  'phone',
+                  'avatar_url',
+                  'notification_email',
+                  'notification_sms',
+                  'notification_push',
+                  'stripe_id',
+                  'apple_id',
+                  'docusign_envelope',
+                  'customer_status',
+                  'referal_code',
+                  'notification_text_magic',
+                  'is_phone_verified',
+                  'is_new',
+                  'is_blocked',
+                  )
 
 
 class ValetGetSerializer(ModelSerializer):
 
     class Meta:
         model = Valet
-        fields = ('first_name', 'last_name', 'email', 'phone', 'avatar_url', 'notification_email',
-                  'notification_sms', 'notification_push', 'stripe_id', 'apple_id', 'docusign_envelope',
-                  'valet_experience', 'valet_personal_phone', 'notification_only_working_hours', 'notification_shift',
-                  'notification_appointment', 'valet_reaction_time', 'valet_available_not_on_call', 'city',
-                  'feedback_popup_show_date', 'emergency_name', 'license')
+        fields = ('first_name',
+                  'last_name',
+                  'email',
+                  'phone',
+                  'avatar_url',
+                  'notification_email',
+                  'notification_sms',
+                  'notification_push',
+                  'stripe_id',
+                  'apple_id',
+                  'docusign_envelope',
+                  'valet_experience',
+                  'valet_personal_phone',
+                  'notification_only_working_hours',
+                  'notification_shift',
+                  'notification_appointment',
+                  'valet_reaction_time',
+                  'valet_available_not_on_call',
+                  'city',
+                  'feedback_popup_show_date',
+                  'emergency_name',
+                  'license',
+                  )
