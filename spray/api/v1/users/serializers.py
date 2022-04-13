d0d085cb6262b71d0b9fbae91fb4e7075c722d83
Serhii.Klimushyn <serhii.klimushyn@smissltd.com>
2022-04-13 17:25:20 +0300

    add get-token

---
+++ spray/api/v1/users/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import update_last_login
+from rest_framework import serializers
+from spray.users.models import User
+from rest_framework_jwt.settings import api_settings
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+
+class UserTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get("email", None)
+        user = User.objects.get(email=email)
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email is not found.'
+            )
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_login(None, user)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given email does not exists'
+            )
+        return {
+            'email': user.email,
+            'token': jwt_token
+        }
--- spray/api/v1/users/views.py
+++ spray/api/v1/users/views.py
@@ -1,12 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from spray.api.v1.users.serializers import UserTokenSerializer
 
 
 class HelloView(APIView):
-
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+
+class UserGetTokenView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserTokenSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'email': serializer.data['email'],
+            'token': serializer.data['token'],
+            'token_type': 'Bearer'
+        }
+        status_code = status.HTTP_200_OK
+        return Response(response, status=status_code)
