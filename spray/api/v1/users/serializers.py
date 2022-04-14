59cab20c278223425a77f442f21a3b40de896705
Serhii.Klimushyn <serhii.klimushyn@smissltd.com>
2022-04-14 09:46:04 +0300

    add convert access to auth token

--- spray/api/v1/users/serializers.py
+++ spray/api/v1/users/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import update_last_login
 from rest_framework import serializers
 from spray.users.models import User
 from rest_framework_jwt.settings import api_settings
@@ -9,34 +8,21 @@ JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
 
 class UserTokenSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=255)
+    email = serializers.CharField(max_length=255, read_only=True)
     access_token = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
 
     def validate(self, data):
-        email = data.get("email", None)
         access_token = data.get("access_token", None)
         try:
-            user = User.objects.get(email=email)
-            user_access_token = AccessToken.objects.get(user_id=user.pk)
-            if str(access_token) != str(user_access_token):
-                raise ValueError
+            user = User.objects.get(pk=AccessToken.objects.get(token=access_token).user_id)
             payload = JWT_PAYLOAD_HANDLER(user)
             jwt_token = JWT_ENCODE_HANDLER(payload)
-        except ValueError:
-            raise serializers.ValidationError(
-                "Wrong access token."
-            )
         except (User.DoesNotExist, AccessToken.DoesNotExist):
             raise serializers.ValidationError(
-                        "Unable to log in with provided credentials."
+                "Unable to get token."
             )
-
         return {
             'email': user.email,
             'token': jwt_token
         }
-
-
-
-# AccessToken.objects.get(user_id=user.pk)
