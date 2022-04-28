870958e284a424b9c102839ee8b55b03bd7479ce
Serhii.Klimushyn <serhii.klimushyn@smissltd.com>
2022-04-28 16:39:19 +0300

    change schedule urls

--- spray/api/v1/schedule/urls.py
+++ spray/api/v1/schedule/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from spray.api.v1.schedule import views
 
 # ----------------------------------------------------------------------- #
 # ----------------------------------------------------------------------- #
 
+router = routers.DefaultRouter()
+router.register('valet-schedule', views.ValetScheduleViewSet, basename='valet-schedule')
+router.register('additional-time', views.ValetScheduleAdditionalTimeView, basename='additional-time')
 
 urlpatterns = [
-    path('valet-schedule/', views.ValetScheduleViewSet, name='valet-schedule'),
-    path('available-valets/', views.AvailableValetsView, name='available-valets'),
-    path('additional-time/', views.ValetScheduleAdditionalTimeView, name='additional_time'),
+    path('', include(router.urls)),
+    path('available-valets/', views.AvailableValetsView.as_view(), name='available-valets'),
 ]
