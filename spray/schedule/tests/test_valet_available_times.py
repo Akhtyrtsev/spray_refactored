979deb0a5c771d50496fb8f82850cfee7b99511f
Serhii.Klimushyn <serhii.klimushyn@smissltd.com>
2022-05-09 22:41:22 +0300

    add schedule tests

--- spray/schedule/tests/test_valet_available_times.py
+++ spray/schedule/tests/test_valet_available_times.py
@@ -1,6 +1,7 @@
 import datetime
 import random
-from unittest import TestCase
+
+from django.test import TestCase
 
 from django.contrib.auth.hashers import make_password
 from django.utils import timezone
@@ -8,7 +9,8 @@ from django.utils import timezone
 from spray.data.timezones import TIMEZONE_OFFSET
 from spray.schedule.models import ValetScheduleDay
 from spray.users.models import Valet
-from spray.utils.get_availability_data import get_available_valet
+from spray.utils.get_availability_data import ValetSchedule
+from spray.utils.parse_schedule import sort_time, format_time, get_time_range
 
 WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
@@ -18,17 +20,22 @@ CORRECT_HOURS1 = [
     '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM',
     '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM'
 ]
+CORRECT_HOURS2 = ['09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM']
+CORRECT_HOURS3 = ['12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM']
+CORRECT_HOURS4 = ['01:00 PM']
+CORRECT_HOURS5 = [
+    '00:00 AM', '00:30 AM', '01:00 AM', '01:30 AM', '02:00 AM', '02:30 AM', '03:00 AM', '03:30 AM', '04:00 AM',
+    '04:30 AM', '05:00 AM', '05:30 AM', '06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM',
+    '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM',
+    '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM',
+    '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM', '10:00 PM',
+    '10:30 PM', '11:00 PM', '11:30 PM'
+]
 
 
 class ValetAvailableTimesTestCase(TestCase):
     def setUp(self):
         valet_quantity = 10
-        try:
-            for i in range(valet_quantity):
-                user = Valet.objects.get(email=f"valet{i}@test.com")
-                user.delete()
-        except Valet.DoesNotExist:
-            pass
         """
         Creating Valet's instances
         """
@@ -52,25 +59,63 @@ class ValetAvailableTimesTestCase(TestCase):
     def test_working_days_creation(self):
         self.assertEqual(Valet.objects.last().working_days.count(), 7)
 
-    def test_available_times_valet(self):
+    def test_available_times_valet_1(self):
         """
         check when working_hours is None
         """
         valet = Valet.objects.order_by("?").first()
         now = timezone.now() + datetime.timedelta(hours=TIMEZONE_OFFSET[valet.city])
         date = now + datetime.timedelta(days=1)
-        hours = get_available_valet(valet, date)
+        hours = ValetSchedule.get_available_valet(valet, date)
         self.assertEqual(hours, [])
 
+    def test_available_times_valet_2(self):
         """
         check when working_hours from 09:00 AM to 05:00 PM
         """
+        valet = Valet.objects.order_by("?").first()
+        now = timezone.now() + datetime.timedelta(hours=TIMEZONE_OFFSET[valet.city])
+        date = now + datetime.timedelta(days=1)
         for day in valet.working_days.all():
             day.working_hours = {
                 "data": [{"start": "09:00 AM", "to": "05:00 PM"}]}
             day.is_working = True
             day.save()
-        get_working_hours = get_available_valet(valet, date)
+        get_working_hours = ValetSchedule.get_available_valet(valet, date)
         hours = set(get_working_hours)
         correct_hours_1 = set(CORRECT_HOURS1)
         self.assertEqual(hours, correct_hours_1)
+
+
+class TimesFunctionsTestCase(TestCase):
+    def test_sort_time(self):
+        hours1 = sort_time(set(CORRECT_HOURS1))
+        hours2 = sort_time(CORRECT_HOURS1[::-1])
+        correct_hours2 = CORRECT_HOURS1[:]
+        random.shuffle(correct_hours2)
+        hours3 = sort_time(correct_hours2)
+        self.assertEqual(hours1, CORRECT_HOURS1)
+        self.assertEqual(hours2, CORRECT_HOURS1)
+        self.assertEqual(hours3, CORRECT_HOURS1)
+
+    def test_format_time(self):
+        self.assertEqual(format_time(date=datetime.datetime.strptime('00:00', '%H:%M')), '00:00 AM')
+        self.assertEqual(format_time(date=datetime.datetime.strptime('06:00', '%H:%M')), '06:00 AM')
+        self.assertEqual(format_time(date=datetime.datetime.strptime('12:00', '%H:%M')), '12:00 PM')
+        self.assertEqual(format_time(date=datetime.datetime.strptime('12:30', '%H:%M')), '12:30 PM')
+        self.assertEqual(format_time(date=datetime.datetime.strptime('13:00', '%H:%M')), '01:00 PM')
+        self.assertEqual(format_time(date=datetime.datetime.strptime('23:00', '%H:%M')), '11:00 PM')
+
+    def test_get_time_range(self):
+        time_range1 = get_time_range({"data": [{"start": "09:00 AM", "to": "05:00 PM"}]})
+        time_range2 = get_time_range({"data": [{"start": "09:00 AM", "to": "12:30 PM"}]})
+        time_range3 = get_time_range({"data": [{"start": "12:00 PM", "to": "03:30 PM"}]})
+        time_range4 = get_time_range({"data": [{"start": "01:00 PM", "to": "01:30 PM"}]})
+        time_range5 = get_time_range({"data": [{"start": "00:00 AM", "to": "00:00 AM"}]})
+        time_range6 = get_time_range({"data": [{"start": None, "to": None}]})
+        self.assertEqual(time_range1, CORRECT_HOURS1)
+        self.assertEqual(time_range2, CORRECT_HOURS2)
+        self.assertEqual(time_range3, CORRECT_HOURS3)
+        self.assertEqual(time_range4, CORRECT_HOURS4)
+        self.assertEqual(time_range5, CORRECT_HOURS5)
+        self.assertEqual(time_range6, [])
