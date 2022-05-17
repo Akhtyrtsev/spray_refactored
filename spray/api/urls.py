d2a5f6a86c1c69d9fc927a368a80f9dc65d0d8cd
judah059 <ppddee777@gmail.com>
2022-05-17 14:46:03 +0300

    appointment cancel

--- spray/api/urls.py
+++ spray/api/urls.py
@@ -3,8 +3,6 @@ from spray.api.v1.authentication import urls as authentication_urls
 from spray.api.v1.users import urls as users_urls
 from spray.api.v1.stripe_system import urls as payment_urls
 from spray.api.v1.subscriptions import urls as subscriptions_urls
-from spray.api.v1.schedule import urls as schedule_urls
-
 from spray.api.v1.device import urls as device_urls
 from spray.api.v1.notifications import urls as notification_urls
 from spray.api.v1.booking import urls as booking_urls
@@ -26,9 +24,6 @@ urlpatterns = [
     # -------------------------- subscriptions -------------------------- #
     # ------------------------------------------------------------- #
     path("", include(subscriptions_urls)),
-    # -------------------------- schedule -------------------------- #
-    # ------------------------------------------------------------- #
-    path("", include(schedule_urls)),
     # -------------------------- devices -------------------------- #
     # ------------------------------------------------------------- #
     path("", include(device_urls)),
